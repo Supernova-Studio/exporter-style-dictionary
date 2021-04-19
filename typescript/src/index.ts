284293ddca95e141b802c2760f304d7ad6ac336d
Jiří Třečák <jiri@supernova.studio>
2021-04-19 20:53:13 +0200

    Generate groups

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,13 +1,35 @@
 /**
- * Base function template
+ * Format object to pretty JSON
  */
-Pulsar.registerFunction("objectToPrettyJsonString", function (object: Object) {
+Pulsar.registerFunction("objectToPrettyJson", function (object: Object) {
   return JSON.stringify(object, null, 2);
 })
 
 /**
- * Base function template
+ * Generate style dictionary tree
  */
-Pulsar.registerFunction("generateStyleDictionaryTree", function (allTokens: BorderToken[]) {
-  return "Hello from Typescript"
+Pulsar.registerFunction("generateStyleDictionaryTree", function (tokens: Token[], rootGroup: TokenGroup) {
+
+  let writeRoot = {}
+  let result = representTree(rootGroup, tokens, writeRoot)
+  return result
 })
+
+
+function representTree(rootGroup: TokenGroup, tokens: Token[], writeObject: Object): Object {
+
+  for (let group of rootGroup.subgroups) {
+    let writeSubObject = {}
+    writeObject[safeGroupName(group.name)] = representTree(group, tokens, writeSubObject)
+  }
+
+  return writeObject
+}
+
+
+function safeGroupName(groupName: string) {
+
+  // Replace spaces with dashes, also change anything non-alphanumeric char to it as well. 
+  // For example, ST&RK Industries will be changed to st-rk-industries
+  return groupName.replace(/\W+/g, '-').toLowerCase()
+}
